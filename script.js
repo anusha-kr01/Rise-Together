a0b5a1bd90874ff32c5c0d2194aeaffa9441224e
mergesort <anushakr719@gmail.com>
2024-12-08 15:31:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * Redirect to the specified URL when a help box is clicked.
+ * @param {string} url - The URL to redirect to.
+ */
+function redirectTo(url) {
+    window.location.href = url;
+}
